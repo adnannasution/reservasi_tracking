7f69340777d4d844dcfcd87b49726c862e220380
adnannasution <adnan.buyung01@gmail.com>
2026-04-12 11:23:43 +0700

    tambah kapasistas upload

--- server.js
+++ server.js
@@ -283,7 +283,7 @@ app.use(cors({
 }));
 
 // Kurangi limit payload dari 50mb ke 10mb
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ─────────────────────────────────────────────
